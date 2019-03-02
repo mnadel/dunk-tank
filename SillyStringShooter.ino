b8b80b524b7d14cd1d37e820982bdf4030112e75
Michael Nadel <michael.nadel@drip.com>
2019-03-02 09:59:57 -0600

    handle two digit integers from ir remote

--- SillyStringShooter.ino
+++ SillyStringShooter.ino
@@ -16,38 +16,13 @@ const int DEACTIVATE_ANGLE = 0;
 IRrecv irrecv(IRRECV_PIN);
 decode_results results;
 
-int translateIR() {
-  switch (results.value)
-  {
-    case 0xFFA25D: Serial.println("PWER"); break;
-    case 0xFFE21D: Serial.println("FUNC/STP"); break;
-    case 0xFF629D: Serial.println("V+"); break;
-    case 0xFF22DD: Serial.println("FFR"); break;
-    case 0xFF02FD: Serial.println("PLY"); break;
-    case 0xFFC23D: Serial.println("FFD"); break;
-    case 0xFFE01F: Serial.println("DWN"); break;
-    case 0xFFA857: Serial.println("V-"); break;
-    case 0xFF906F: Serial.println("UP"); break;
-    case 0xFF9867: Serial.println("EQ"); break;
-    case 0xFFB04F: Serial.println("ST/RPT"); break;
-    case 0xFF6897: Serial.println("0"); return 0;
-    case 0xFF30CF: Serial.println("1"); return 1;
-    case 0xFF18E7: Serial.println("2"); return 2;
-    case 0xFF7A85: Serial.println("3"); return 3;
-    case 0xFF10EF: Serial.println("4"); return 4;
-    case 0xFF38C7: Serial.println("5"); return 5;
-    case 0xFF5AA5: Serial.println("6"); return 6;
-    case 0xFF42BD: Serial.println("7"); return 7;
-    case 0xFF4AB5: Serial.println("8"); return 8;
-    case 0xFF52AD: Serial.println("9"); return 9;
-    case 0xFFFFFFFF: Serial.println("REPEAT"); break;
-    default:
-      Serial.println(results.value, HEX);
-  }
+// state management
+const int KEYPRESS_THRESHOLD_MS = 750;
+long lastReceiveTime;
+int lastValueReceived = -1;
 
-  delay(500); // Do not get immediate repeat
-  return -1;
-}
+// math challenge
+const int ANSWER = 14;
 
 void setup()
 {
@@ -57,6 +32,26 @@ void setup()
   servo.attach(SERVO_PIN);
   irrecv.enableIRIn();
   pinMode(LED_BUILTIN, OUTPUT);
+
+  servo.write(DEACTIVATE_ANGLE);
+}
+
+unsigned int translateIR() {
+  switch (results.value)
+  {
+    case 0xFF6897: return 0;
+    case 0xFF30CF: return 1;
+    case 0xFF18E7: return 2;
+    case 0xFF7A85: return 3;
+    case 0xFF10EF: return 4;
+    case 0xFF38C7: return 5;
+    case 0xFF5AA5: return 6;
+    case 0xFF42BD: return 7;
+    case 0xFF4AB5: return 8;
+    case 0xFF52AD: return 9;
+  }
+
+  return -1;
 }
 
 void loop()
@@ -65,15 +60,35 @@ void loop()
 
   if (irrecv.decode(&results))
   {
-    int pressed = translateIR();
+    long receiveTime = millis();
+    long timeSinceLastPress = receiveTime - lastReceiveTime;
+    int currValue = translateIR();
+    
+    if (currValue >= 0) {
+      Serial.print("pressed=");
+      Serial.println(currValue);
+      
+      if (timeSinceLastPress <= KEYPRESS_THRESHOLD_MS) {
+        Serial.print("adding prev=");
+        Serial.println(lastValueReceived);
+        
+        currValue = lastValueReceived*10 + currValue;
+      }
 
-    if (pressed == 5) {
-      digitalWrite(LED_BUILTIN, HIGH);
-      Serial.println("triggering");
+      Serial.print("current=");
+      Serial.println(currValue);
 
-      servo.write(ACTIVATE_ANGLE);
-      delay(ACTIVATION_DURATION);
-      servo.write(DEACTIVATE_ANGLE);
+      if (currValue == ANSWER) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        Serial.println("triggering");
+  
+        servo.write(ACTIVATE_ANGLE);
+        delay(ACTIVATION_DURATION);
+        servo.write(DEACTIVATE_ANGLE);
+      }
+
+      lastValueReceived = currValue;
+      lastReceiveTime = receiveTime;
     }
 
     irrecv.resume();
