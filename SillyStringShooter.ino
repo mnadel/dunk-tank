c8e46ddd7471f69e1a6226006e41e3220e6cba7f
Michael Nadel <michael.nadel@drip.com>
2019-03-01 18:45:19 -0600

    use all-caps for constants

--- SillyStringShooter.ino
+++ SillyStringShooter.ino
@@ -1,47 +1,47 @@
-#include <limits.h>
 #include <Servo.h>
 #include "NewPing.h"
 
 // hardware control
-const int echoPin = 11;
-const int triggerPin = 12;
-const int servoPin = 9;
-
-NewPing sonar(triggerPin, echoPin);
-Servo servo;
+const int ECHO_PIN = 11;
+const int TRIGGER_PIN = 12;
+const int SERVO_PIN = 9;
 
 // sonar logic
-const int acquireDist = 12;
-const int reacquireDist = 24;
+const int ACQUIRE_DIST = 12;
+const int REACQUIRE_DIST = 24;
+
+// servo logic
+const int ACTIVATE_ANGLE = 180;
+const int ACTIVATION_DURATION = 750;
+const int DEACTIVATE_ANGLE = 0;
+const int PINGS = 10;
+
+// state
+NewPing sonar(TRIGGER_PIN, ECHO_PIN);
+Servo servo;
 
 bool acquired = false;
 bool searching = true;
 long currDist;
 long prevDist;
 
-// servo logic
-const int activateAngle = 180;
-const int activationDuration = 750;
-const int deactivateAngle = 0;
-const int pings = 5;
-
 void setup() {
   Serial.begin(9600);
-  servo.attach(servoPin);
+  servo.attach(SERVO_PIN);
 }
 
 void loop() {
-  currDist = NewPing::convert_in(sonar.ping_median(pings));
- 
-  acquired = searching && currDist <= acquireDist;
-  searching = currDist > reacquireDist;
+  currDist = NewPing::convert_in(sonar.ping_median(PINGS));
+
+  acquired = searching && currDist <= ACQUIRE_DIST;
+  searching = currDist > REACQUIRE_DIST;
 
   log();
 
   if (acquired) {
-    servo.write(activateAngle);
-    delay(activationDuration);
-    servo.write(deactivateAngle);
+    servo.write(ACTIVATE_ANGLE);
+    delay(ACTIVATION_DURATION);
+    servo.write(DEACTIVATE_ANGLE);
   }
 
   prevDist = currDist;
